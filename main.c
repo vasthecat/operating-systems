a7f81f7c3b8fb821ff914f56dc010624ecc0ef2a
Andrew Guschin <guschin.drew@gmail.com>
2022-01-22 19:51:21 +0400

    Changed indentation (crucial change!)

--- main.c
+++ main.c
@@ -13,30 +13,30 @@
 
 enum brute_mode_t
 {
-  M_RECURSIVE,
-  M_ITERATIVE
+    M_RECURSIVE,
+    M_ITERATIVE
 };
 
 struct config_t
 {
-  char *alphabet;
-  int length;
-  enum brute_mode_t mode;
-  char *hash;
+    char *alphabet;
+    int length;
+    enum brute_mode_t mode;
+    char *hash;
 };
 
 struct task_t
 {
-  char password[PASSWORD_SIZE];
+    char password[PASSWORD_SIZE];
 };
 
 struct queue_t
 {
-  struct task_t tasks[8];
-  int size, capacity;
-  int head, tail;
-  pthread_mutex_t head_mut, tail_mut;
-  sem_t *count, *available;
+    struct task_t tasks[8];
+    int size, capacity;
+    int head, tail;
+    pthread_mutex_t head_mut, tail_mut;
+    sem_t *count, *available;
 };
 struct queue_t queue;
 bool found = false;
@@ -45,212 +45,212 @@ char result[PASSWORD_SIZE];
 void
 queue_init(struct queue_t *queue)
 {
-  queue->size = 0;
-  queue->capacity = 8;
-  queue->head = queue->tail = 0;
+    queue->size = 0;
+    queue->capacity = 8;
+    queue->head = queue->tail = 0;
 
-  // MacOS doesn't support unnamed semaphores
-  if ((queue->count = sem_open("/brute-sem_count", O_CREAT, 0644, 0)) == SEM_FAILED)
-  {
-    printf("Could not initialize 'count' semaphore\n");
-    exit(EXIT_FAILURE);
-  }
-  sem_unlink("/brute-sem_count");
-  if ((queue->available = sem_open("/brute-sem_available", O_CREAT, 0644, queue->capacity)) == SEM_FAILED)
-  {
-    sem_close(queue->count);
-    printf("Could not initialize 'available' semaphore\n");
-    exit(EXIT_FAILURE);
-  }
-  sem_unlink("/brute-sem_available");
+    // MacOS doesn't support unnamed semaphores
+    if ((queue->count = sem_open("/brute-sem_count", O_CREAT, 0644, 0)) == SEM_FAILED)
+    {
+	printf("Could not initialize 'count' semaphore\n");
+	exit(EXIT_FAILURE);
+    }
+    sem_unlink("/brute-sem_count");
+    if ((queue->available = sem_open("/brute-sem_available", O_CREAT, 0644, queue->capacity)) == SEM_FAILED)
+    {
+	sem_close(queue->count);
+	printf("Could not initialize 'available' semaphore\n");
+	exit(EXIT_FAILURE);
+    }
+    sem_unlink("/brute-sem_available");
 
-  pthread_mutex_init(&queue->head_mut, NULL);
-  pthread_mutex_init(&queue->tail_mut, NULL);
+    pthread_mutex_init(&queue->head_mut, NULL);
+    pthread_mutex_init(&queue->tail_mut, NULL);
 }
 
 void
 queue_destroy(struct queue_t *queue)
 {
-  sem_close(queue->count);
-  sem_close(queue->available);
+    sem_close(queue->count);
+    sem_close(queue->available);
 }
 
 void
 queue_push(struct queue_t *queue, struct task_t *task)
 {
-  sem_wait(queue->available);
+    sem_wait(queue->available);
 
-  pthread_mutex_lock(&queue->tail_mut);
-  queue->tasks[queue->tail] = *task;
-  queue->tail = (queue->tail + 1) % queue->capacity;
-  queue->size++;
-  /* printf("Enqued '%s'\n", task->password); */
-  pthread_mutex_unlock(&queue->tail_mut);
+    pthread_mutex_lock(&queue->tail_mut);
+    queue->tasks[queue->tail] = *task;
+    queue->tail = (queue->tail + 1) % queue->capacity;
+    queue->size++;
+    /* printf("Enqued '%s'\n", task->password); */
+    pthread_mutex_unlock(&queue->tail_mut);
   
-  sem_post(queue->count);
+    sem_post(queue->count);
 }
 
 void
 queue_pop(struct queue_t *queue, struct task_t *task)
 {
-  sem_wait(queue->count);
+    sem_wait(queue->count);
 
-  pthread_mutex_lock(&queue->head_mut);
-  *task = queue->tasks[queue->head];
-  queue->head = (queue->head + 1) % queue->capacity;
-  queue->size--;
-  /* printf("Dequed '%s'\n", task->password); */
-  pthread_mutex_unlock(&queue->head_mut);
+    pthread_mutex_lock(&queue->head_mut);
+    *task = queue->tasks[queue->head];
+    queue->head = (queue->head + 1) % queue->capacity;
+    queue->size--;
+    /* printf("Dequed '%s'\n", task->password); */
+    pthread_mutex_unlock(&queue->head_mut);
 
-  sem_post(queue->available);
+    sem_post(queue->available);
 }
 
 void *
 check_password(void *arg)
 {
-  char *hash = (char *) arg;
+    char *hash = (char *) arg;
   
-  while (true)
-  {
-    struct task_t task;
-    queue_pop(&queue, &task);
-    
-    struct crypt_data data;
-    char *hashed = crypt_r(task.password, hash, &data);
-    if (strcmp(hashed, hash) == 0)
+    while (true)
     {
-      printf("Password found: '%s'\n", task.password);
-      found = true;
-      strcpy(result, task.password);
+	struct task_t task;
+	queue_pop(&queue, &task);
+
+	struct crypt_data data;
+	char *hashed = crypt_r(task.password, hash, &data);
+	if (strcmp(hashed, hash) == 0)
+	{
+	    printf("Password found: '%s'\n", task.password);
+	    found = true;
+	    strcpy(result, task.password);
+	}
+	if (found) break;
     }
-    if (found) break;
-  }
-  return NULL;
+    return NULL;
 }
 
 void
 bruteforce_rec(char *password, struct config_t *config, int pos)
 {
-  if (config->length == pos)
-  {
-    struct task_t task;
-    memset(task.password, 0, PASSWORD_SIZE);
-    strcpy(task.password, password);
-    queue_push(&queue, &task);
-  }
-  else
-  {
-    for (int i = 0; config->alphabet[i] != '\0'; ++i)
+    if (config->length == pos)
     {
-      password[pos] = config->alphabet[i];
-      bruteforce_rec(password, config, pos + 1);
+	struct task_t task;
+	memset(task.password, 0, PASSWORD_SIZE);
+	strcpy(task.password, password);
+	queue_push(&queue, &task);
     }
-  }
+    else
+    {
+	for (int i = 0; config->alphabet[i] != '\0'; ++i)
+	{
+	    password[pos] = config->alphabet[i];
+	    bruteforce_rec(password, config, pos + 1);
+	}
+    }
 } 
 
 void
 bruteforce_iter(struct config_t *config)
 {
-  size_t size = strlen(config->alphabet) - 1;
-  int a[config->length];
-  memset(a, 0, config->length * sizeof(int));
+    size_t size = strlen(config->alphabet) - 1;
+    int a[config->length];
+    memset(a, 0, config->length * sizeof(int));
 
-  while (true)
-  {
-    int k;
-    struct task_t task;
-    memset(task.password, 0, PASSWORD_SIZE);
-    for (k = 0; k < config->length; ++k)
-      task.password[k] = config->alphabet[a[k]];
+    while (true)
+    {
+	int k;
+	struct task_t task;
+	memset(task.password, 0, PASSWORD_SIZE);
+	for (k = 0; k < config->length; ++k)
+	    task.password[k] = config->alphabet[a[k]];
 
-    queue_push(&queue, &task);
+	queue_push(&queue, &task);
     
-    for (k = config->length - 1; (k >= 0) && (a[k] == size); --k)
-      a[k] = 0;
-    if (k < 0) break;
-    a[k]++;
-  }
+	for (k = config->length - 1; (k >= 0) && (a[k] == size); --k)
+	    a[k] = 0;
+	if (k < 0) break;
+	a[k]++;
+    }
 }
 
 void
 parse_opts(struct config_t *config, int argc, char *argv[])
 {
-  int opt;
-  opterr = 1;
-  while ((opt = getopt(argc, argv, "ira:l:h:")) != -1)
-  {
-    switch (opt)
+    int opt;
+    opterr = 1;
+    while ((opt = getopt(argc, argv, "ira:l:h:")) != -1)
     {
-    case 'i':
-      config->mode = M_ITERATIVE;
-      break;
-    case 'r':
-      config->mode = M_RECURSIVE;
-      break;
-    case 'a':
-      config->alphabet = optarg;
-      break;
-    case 'l':
-      config->length = atoi(optarg);
-      break;
-    case 'h':
-      config->hash = optarg;
-      break;
-    default:
-      exit(1);
-      break;
+	switch (opt)
+	{
+	case 'i':
+	    config->mode = M_ITERATIVE;
+	    break;
+	case 'r':
+	    config->mode = M_RECURSIVE;
+	    break;
+	case 'a':
+	    config->alphabet = optarg;
+	    break;
+	case 'l':
+	    config->length = atoi(optarg);
+	    break;
+	case 'h':
+	    config->hash = optarg;
+	    break;
+	default:
+	    exit(1);
+	    break;
+	}
     }
-  }
 }
 
 int
 main(int argc, char *argv[])
 {
-  struct config_t config = {
-    .alphabet = "abcd",
-    .length = 3,
-    .mode = M_ITERATIVE,
-    .hash = "hiN3t5mIZ/ytk", // hi + abcd
-  };
-  parse_opts(&config, argc, argv);
-  queue_init(&queue);
+    struct config_t config = {
+	.alphabet = "abcd",
+	.length = 3,
+	.mode = M_ITERATIVE,
+	.hash = "hiN3t5mIZ/ytk", // hi + abcd
+    };
+    parse_opts(&config, argc, argv);
+    queue_init(&queue);
 
-  int cpu_count = sysconf(_SC_NPROCESSORS_ONLN);
+    int cpu_count = sysconf(_SC_NPROCESSORS_ONLN);
 
-  pthread_t threads[cpu_count];
-  for (int i = 0; i < cpu_count; ++i)
-  {
-    pthread_create(&threads[i], NULL, check_password, (void *) config.hash);
-  }
+    pthread_t threads[cpu_count];
+    for (int i = 0; i < cpu_count; ++i)
+    {
+	pthread_create(&threads[i], NULL, check_password, (void *) config.hash);
+    }
 
-  bool found = false;
-  char password[config.length + 1];
-  password[config.length] = '\0';
-  switch (config.mode)
-  {
-  case M_ITERATIVE:
-    bruteforce_iter(&config);
-    break;
-  case M_RECURSIVE:
-    bruteforce_rec(password, &config, 0);
-    break;
-  }
+    bool found = false;
+    char password[config.length + 1];
+    password[config.length] = '\0';
+    switch (config.mode)
+    {
+    case M_ITERATIVE:
+	bruteforce_iter(&config);
+	break;
+    case M_RECURSIVE:
+	bruteforce_rec(password, &config, 0);
+	break;
+    }
 
-  for (int i = 0; i < cpu_count; ++i)
-  {
-    pthread_join(threads[i], NULL);
-  }
+    for (int i = 0; i < cpu_count; ++i)
+    {
+	pthread_join(threads[i], NULL);
+    }
 
-  queue_destroy(&queue);
+    queue_destroy(&queue);
 
-  if (found)
-  {
-    printf("Found password: '%s'\n", result);
-  }
-  else
-  {
-    printf("Password not found\n");
-  }
+    if (found)
+    {
+	printf("Found password: '%s'\n", result);
+    }
+    else
+    {
+	printf("Password not found\n");
+    }
 
-  return 0;
+    return 0;
 }
